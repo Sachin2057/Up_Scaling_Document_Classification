84491357184ccd4d88f1496b226fe67a18735c46
Sachin Acharya <sachinacharya44@gmail.com>
2024-05-10 16:38:19 +0545

    add training and validation configuration

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+DATASET_DIRECTORY = "Raw"
+CITIZENSHIP_DIRECTORY = os.path.join(DATASET_DIRECTORY, "Citizenship")
+PASSPORT_DIRECTORY = os.path.join(DATASET_DIRECTORY, "Passport")
+OTHER_DIRECTORY = os.path.join(DATASET_DIRECTORY, "Others")
+LICENSE_DIRECTORY = os.path.join(DATASET_DIRECTORY, "License")
+
+
+TRAIN_SAVE_DIRECTORY = os.path.join("Dataset", "Train")
+VALID_SAVE_DIRECTORY = os.path.join("Dataset", "Valid")
+
+classes = [
+    CITIZENSHIP_DIRECTORY,
+    PASSPORT_DIRECTORY,
+    LICENSE_DIRECTORY,
+    # OTHER_DIRECTORY,
+]
+
+######Model parameters
+batch_size = 16
+classes_name = ["Citizenship", "Passport", "License"]
+num_classes = 3
