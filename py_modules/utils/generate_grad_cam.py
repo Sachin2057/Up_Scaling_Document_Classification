9fa8ae4abfd52a5722edc6e11251c754b5e26736
Sachin Acharya <sachinacharya44@gmail.com>
2024-05-10 16:34:49 +0545

    add support for pillow images

--- py_modules/utils/generate_grad_cam.py
+++ py_modules/utils/generate_grad_cam.py
@@ -22,13 +22,25 @@ from PIL import Image
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def generate_grad_cam(checkpoint, input_image_path, target):
-    image = cv2.imread(input_image_path)
-    print(image)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image=cv2.resize(image,(224,224))
-    image = np.float32(image) / 255
-    print(image)    
+def generate_grad_cam(model, input_image_path, target):
+    """
+    Generate Grad-CAM visualization for a given input image and target class.
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        The PyTorch model for which Grad-CAM is generated.
+    input_image_path : str or np.ndarray
+         Path to the input image file or
+            numpy array representing the input image
+    target : int
+        Target class index for which Grad-CAM is computed.
+
+    Returns
+    -------
+    np.ndarray
+         Grad-CAM visualization overlaid on the input image.
+    """
     normalize = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
     )
@@ -40,10 +52,22 @@ def generate_grad_cam(checkpoint, input_image_path, target):
             normalize,
         ]
     )
+    if type(input_image_path) is str:
+        image = cv2.imread(input_image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (224, 224))
+        image = np.float32(image) / 255
+
+    else:
+        image = input_image_path
+        image = np.array(image)
+        image = cv2.resize(image, (224, 224))
+        image = np.float32(image) / 255
+
     image_tensor = preprocessing(image).to(device)
     image_tensor = image_tensor[None, :, :, :]
-    model = ClassificationModel(num_classes=config.num_classes).to(device)
-    model.load_state_dict(torch.load(checkpoint))
+    # model = ClassificationModel(num_classes=config.num_classes).to(device)
+    # model.load_state_dict(torch.load(checkpoint))
     target_layer = model.resnet.layer4
     # cam = GradCAM(model=model, target_layers=target_layer)
     targets = [
@@ -57,7 +81,4 @@ def generate_grad_cam(checkpoint, input_image_path, target):
         images = np.hstack((np.uint8(255 * image), cam, cam_image))
         images = Image.fromarray(images)
         images = np.array(images)
-        print(images.shape)
-        print(image)
-        cv2.imshow("window", images)
-        cv2.waitKey(0)
+        return images
