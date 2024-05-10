278ff0ff3ef91b4cad2ef6be4e6426c211366d13
Sachin Acharya <sachinacharya44@gmail.com>
2024-05-10 16:45:58 +0545

    fix augmentaion pipeline

--- generate_dataset.py
+++ generate_dataset.py
@@ -4,22 +4,7 @@ import random
 import albumentations as A
 import cv2
 from sklearn.model_selection import train_test_split
-
-DATASET_DIRECTORY = "Raw"
-CITIZENSHIP_DIRECTORY = os.path.join(DATASET_DIRECTORY, "Citizenship")
-PASSPORT_DIRECTORY = os.path.join(DATASET_DIRECTORY, "Passport")
-OTHER_DIRECTORY = os.path.join(DATASET_DIRECTORY, "License")
-LICENSE_DIRECTORY = os.path.join(DATASET_DIRECTORY, "License")
-
-
-TRAIN_SAVE_DIRECTORY = os.path.join("Dataset", "Train")
-VALID_SAVE_DIRECTORY = os.path.join("Dataset", "Valid")
-
-classes = [
-    CITIZENSHIP_DIRECTORY,
-    PASSPORT_DIRECTORY,
-    LICENSE_DIRECTORY,
-]
+import config
 
 
 def get_main_augmentation():
@@ -45,10 +30,9 @@ def get_valid_augmentation():
     valid_transform = A.Compose(
         [
             A.ColorJitter(brightness=(0.8, 1.2), contrast=0, saturation=0, hue=0),
-            A.ColorJitter(brightness=0, contrast=(1, 3), saturation=0, hue=0),
-            A.ColorJitter(brightness=0, contrast=0, saturation=(0, 5), hue=0),
+            A.ColorJitter(brightness=0, contrast=(0.5, 1.5), saturation=0, hue=0),
+            A.ColorJitter(brightness=0, contrast=0, saturation=(0.5, 1.5), hue=0),
             A.ColorJitter(brightness=0, contrast=0, saturation=0, hue=0.5),
-            A.Posterize(num_bits=(0, 5), p=0.7),
         ]
     )
     return valid_transform
@@ -60,7 +44,7 @@ def image_augmentation(image_path, transformation):
 
     Parameters
     ----------
-    image_path : str
+    image_path : str or nd_array
         path of the image
     transformation : transfomation
         Transformation to be applied
@@ -70,41 +54,63 @@ def image_augmentation(image_path, transformation):
     image:numpy_array
         Augmented image
     """
-    image = cv2.imread(image_path)
+    if type(image_path) is str:
+        image = cv2.imread(image_path)
+    else:
+        image = image_path
     augmented = transformation(image=image)
     augmented_image = augmented["image"]
     return augmented_image
 
 
 def generate_dataset():
-    for ix, class_dir in enumerate(classes):
+    """
+    Load images from the raw folders split the images into train split
+    add augmentaion and save in training directory
+    """
+    for ix, class_dir in enumerate(config.classes):
         files = os.listdir(class_dir)
-        num_samples_to_augment = int(0.4 * len(files))
-        indices_to_augment = random.sample(range(len(files)), num_samples_to_augment)
-        for index in indices_to_augment:
-            image_path = os.path.join(class_dir, files[index])
-            transforamtion = get_main_augmentation()
-            augmented_image = image_augmentation(
-                image_path=image_path, transformation=transforamtion
-            )
-            save_path = os.path.join(class_dir, f"{files[index]}_augmented.jpg")
-            cv2.imwrite(save_path, augmented_image)
-        files = os.listdir(class_dir)
+        # num_samples_to_augment = int(0.4 * len(files))
+        # indices_to_augment = random.sample(range(len(files)), num_samples_to_augment)
+        # for index in indices_to_augment:
+        #     image_path = os.path.join(class_dir, files[index])
+        #     transforamtion = get_main_augmentation()
+        #     augmented_image = image_augmentation(
+        #         image_path=image_path, transformation=transforamtion
+        #     )
+        #     save_path = os.path.join(class_dir, f"{files[index]}_augmented.jpg")
+        #     cv2.imwrite(save_path, augmented_image)
         if len(files) != 0:
             train, test = train_test_split(files, test_size=0.2)
             for image in train:
                 orginal_path = os.path.join(class_dir, image)
-                save_path = os.path.join(TRAIN_SAVE_DIRECTORY, f"{ix}-{image}.jpg")
-                shutil.move(orginal_path, save_path)
+                transforamtion = get_main_augmentation()
+                augmented_image = image_augmentation(
+                    image_path=orginal_path, transformation=transforamtion
+                )
+                save_path_orginal = os.path.join(
+                    config.TRAIN_SAVE_DIRECTORY, f"{ix}-{image}.jpg"
+                )
+                save_path_augmented = os.path.join(
+                    config.TRAIN_SAVE_DIRECTORY, f"{ix}-{image}_augmented.jpg"
+                )
+                cv2.imwrite(save_path_augmented, augmented_image)
+                shutil.move(orginal_path, save_path_orginal)
             for image in test:
                 orginal_path = os.path.join(class_dir, image)
+                main_transforamtion = get_main_augmentation()
                 transforamtion = get_valid_augmentation()
                 augmented_image = image_augmentation(
-                    orginal_path, transformation=transforamtion
+                    orginal_path, transformation=main_transforamtion
                 )
-                save_path = os.path.join(VALID_SAVE_DIRECTORY, f"{ix}-{image}.jpg")
+                augmented_image = image_augmentation(
+                    augmented_image, transformation=transforamtion
+                )
+                save_path = os.path.join(
+                    config.VALID_SAVE_DIRECTORY, f"{ix}-{image}.jpg"
+                )
                 save_augmented_path = os.path.join(
-                    VALID_SAVE_DIRECTORY, f"{ix}-{image}_augmented.jpg"
+                    config.VALID_SAVE_DIRECTORY, f"{ix}-{image}_augmented.jpg"
                 )
                 cv2.imwrite(save_augmented_path, augmented_image)
                 shutil.move(orginal_path, save_path)
